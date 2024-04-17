1a10d2b8631c2f1c43af864db9f4afeb9fd3f407
mrkadirov2005 <muzaffar571181@gmail.com>
2024-04-17 14:18:33 +0500

    corrected missing types

--- src/app/components/github_netlify/page.tsx
+++ src/app/components/github_netlify/page.tsx
@@ -18,7 +18,7 @@ const Button=styled.button`
     `
 
  function Git_Net({text:text}:PROPS) {
-    const info=useSelector(background)
+    const info:boolean=useSelector(background)
     
   return (
     <Button info={info}>{text}</Button>
